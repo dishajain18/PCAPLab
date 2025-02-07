83f9ccf1636746e54931eb351e5833c6759ba811
Disha Jain <dishajain1802@gmail.com>
2025-02-07 15:27:50 +0530

    Update q2.cu

--- Lab5/q2.cu
+++ Lab5/q2.cu
@@ -2,13 +2,13 @@
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
-__global__ void mykernel(int * A,int * B,int * C,int * N)
+__global__ void mykernel(int * A,int * B,int * C,int  N)
 {
 	int bng = blockIdx.x; //block no. in grid
 	int ntpd = blockDim.x; //no.of threads per block
 	int tnb = threadIdx.x; //thread no. in block or local thread id
 	int gtid = bng * ntpd + tnb;  
-	if(gtid < *N)
+	if(gtid < N)
 		C[gtid] = A[gtid] + B[gtid];
 }
 
@@ -18,17 +18,15 @@ int main()
 	int *d_A, *d_B, *d_C;
 	printf("Enter no. of elements: ");
 	int  N;
-	int *d_N;
 	scanf("%d",&N);
 	int S = N * sizeof(int);
 	A = (int*)malloc(S);
 	B = (int*)malloc(S);
 	C = (int*)malloc(S);
 
-    cudaMalloc((void**)&d_A , S);
+    	cudaMalloc((void**)&d_A , S);
 	cudaMalloc((void**)&d_B , S);
 	cudaMalloc((void**)&d_C , S);
-	cudaMalloc((void**)&d_N , sizeof(int)); //no. of threads
 
 	printf("Enter elements in A: ");
 	for(int i=0; i<N; i++)
@@ -44,9 +42,8 @@ int main()
 
 	cudaMemcpy(d_A,A,S,cudaMemcpyHostToDevice);
 	cudaMemcpy(d_B,B,S,cudaMemcpyHostToDevice);
-	cudaMemcpy(d_N,&N,sizeof(int),cudaMemcpyHostToDevice);
 
-	mykernel<<<ceil(N/256.0),256>>>(d_A,d_B,d_C,d_N);
+	mykernel<<<ceil(N/256.0),256>>>(d_A,d_B,d_C,N);
 
 	cudaMemcpy(C,d_C,S,cudaMemcpyDeviceToHost);
 
